9ae1b44eab526b2439723789a2d8fa1e006e8117
Slav Tsekov <slav.tsekov@digitall.com>
2023-08-17 13:59:14 +0300

    Init app

---
+++ src/ui/components/DoorDetail.test.tsx
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+import { Door } from '@/models/Door';
+import { DoorDetail } from './DoorDetail';
+
+const door: Door = {
+  id: '63f637c9f3c48a124616044b',
+  name: 'Building Main Entrance',
+  buildingName: 'Bahnhofstrasse 10A',
+  connectionType: 'wired',
+  connectionStatus: 'offline',
+  lastConnectionStatusUpdate: '2023-02-22T03:00:11.853Z',
+};
+
+describe('DoorDetail', () => {
+  it('should render correctly', () => {
+    const { container } = render(<DoorDetail door={door} />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
---
+++ src/ui/layout/AppBarLogo.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import Box from '@mui/material/Box';
+
+export function AppBarLogo() {
+  return (
+    <Box sx={{ display: { xs: 'none', md: 'flex' }, mr: 2 }}>
+      <Link href="/">
+        <Image
+          src="/logo.png"
+          alt="dormakaba logo"
+          width="269"
+          height="30"
+          priority
+        />
+      </Link>
+    </Box>
+  );
+}
